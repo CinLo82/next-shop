40e8ece0012a6301b304f4a9f3250fbefe05f60d
Cin💜 Lo <cinlos3382@gmail.com>
2023-03-07 00:01:39 -0300

    instalando mas dependencias para la config de next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
+const { redirect } = require('next/dist/server/api-utils')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    customKey: 'customValue',
+  },
+  compress: true,
+  async redirects() {
+    return [
+      {
+        source: '/hola',
+        destination: 'https://gndx.dev',
+        permanent:true,  
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
